35c35f5e92bb7217e99522c3a0f2f5f6bf276b35
gutovianna88 <70817035+gutovianna88@users.noreply.github.com>
2020-09-05 17:15:59 -0300

    Alteração em funções para correção de Maior

--- ListaDeExercicios/EstruturaDeDecisao/1.py
+++ ListaDeExercicios/EstruturaDeDecisao/1.py
@@ -4,8 +4,8 @@ Faça um Programa que peça dois números e imprima o maior deles
 
 """
 
-primeiro_numero = input("Primeiro numero?");
-segundo_numero = input("segundo numero?");
+primeiro_numero = float(input("Primeiro numero?"));
+segundo_numero = float(input("segundo numero?"));
 
 if (primeiro_numero > segundo_numero):
 	print(primeiro_numero);
