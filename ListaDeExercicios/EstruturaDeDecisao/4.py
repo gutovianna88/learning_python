eb1b1cb1c5766fb49d322ecf2195f2a8acd4da4e
gutovianna88 <70817035+gutovianna88@users.noreply.github.com>
2020-09-12 11:06:42 -0300

    Inclusão do exercício 4

---
+++ ListaDeExercicios/EstruturaDeDecisao/4.py
@@ -0,0 +1,19 @@
+"""
+Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+"""
+letra = input("Digite uma letra para verificar se é vogal ou consoante: ")
+letra = letra.upper()
+if(letra == "A"):
+    print("vogal")
+elif(letra == "E"):
+    print("vogal")
+elif(letra == "I"):
+    print("vogal")
+elif(letra == "O"):
+    print("Vogal")
+elif(letra == "U"):
+    print("vogal")
+else:
+    print("consoante")
+
+
